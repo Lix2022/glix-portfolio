7b648e035714698a699159e6e6f0322e9b0cd50b
Lix2022 <98506225+Lix2022@users.noreply.github.com>
2023-05-26 08:23:35 +0000

    first commit

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,24 +1,26 @@
 import { Container, Row, Col } from "react-bootstrap";
 import { MailchimpForm } from "./MailchimpForm";
-import logo from "../assets/img/logo.svg";
+import logo from "../assets/img/logoFG.svg";
 import navIcon1 from "../assets/img/nav-icon1.svg";
 import navIcon2 from "../assets/img/nav-icon2.svg";
 import navIcon3 from "../assets/img/nav-icon3.svg";
 
-export const Footer = () => {
+import React, { useState, useEffect } from 'react';
+
+{/*export const Footer = () => {
   return (
     <footer className="footer">
       <Container>
         <Row className="align-items-center">
-          <MailchimpForm />
+         {/*<MailchimpForm />
           <Col size={12} sm={6}>
             <img src={logo} alt="Logo" />
           </Col>
           <Col size={12} sm={6} className="text-center text-sm-end">
             <div className="social-icon">
-              <a href="#"><img src={navIcon1} alt="Icon" /></a>
-              <a href="#"><img src={navIcon2} alt="Icon" /></a>
-              <a href="#"><img src={navIcon3} alt="Icon" /></a>
+              <a href="https://www.linkedin.com/in/felix-g-b9aaa6217/" target="_blank"><img src={navIcon1} alt="Icon" /></a>
+              <a href="https://www.facebook.com/FelixG28" target="_blank"><img src={navIcon2} alt="Icon" /></a>
+              <a href="https://www.instagram.com/glix29/"target="_blank"><img src={navIcon3} alt="Icon" /></a>
             </div>
             <p>Copyright 2023. All Rights Reserved</p>
           </Col>
@@ -26,4 +28,52 @@ export const Footer = () => {
       </Container>
     </footer>
   )
-}
+}*/}
+
+
+export const Footer = () => {
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth <= 768); // Adjust the value as per your needs
+    };
+
+    window.addEventListener('resize', handleResize);
+    handleResize(); // Check the initial width on mount
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
+
+  return (
+    <footer className="footer">
+      <Container>
+        <Row className="align-items-center">
+          {/* <MailchimpForm /> */}
+          {!isMobile && (
+            <Col size={12} sm={6}>
+              <img src={logo} alt="Logo" />
+            </Col>
+          )}
+          <Col size={12} sm={6} className="text-center text-sm-end">
+            <div className="social-icon"> 
+              <a href="https://www.linkedin.com/in/felix-g-b9aaa6217/" target="_blank">
+                <img src={navIcon1} alt="Icon" />
+              </a>
+              <a href="https://www.facebook.com/FelixG28" target="_blank">
+                <img src={navIcon2} alt="Icon" />
+              </a>
+              <a href="https://www.instagram.com/glix29/" target="_blank">
+                <img src={navIcon3} alt="Icon" />
+              </a>
+            </div>
+            <p>Copyright 2023. All Rights Reserved</p>
+          </Col>
+        </Row>
+      </Container>
+    </footer>
+  );
+};
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import { Navbar, Nav, Container } from "react-bootstrap";
-import logo from '../assets/img/logo.svg';
+import logo from '../assets/img/logoFG.svg';
 import navIcon1 from '../assets/img/nav-icon1.svg';
 import navIcon2 from '../assets/img/nav-icon2.svg';
 import navIcon3 from '../assets/img/nav-icon3.svg';
@@ -34,10 +34,10 @@ export const NavBar = () => {
 
   return (
     <Router>
-      <Navbar expand="md" className={scrolled ? "scrolled" : ""}>
+      <Navbar expand="sm" className={scrolled ? "scrolled" : ""}>
         <Container>
           <Navbar.Brand href="/">
-            <img src={logo} alt="Logo" />
+          <img src={logo} alt="Logo" className="logo-img" />
           </Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav">
             <span className="navbar-toggler-icon"></span>
@@ -51,9 +51,9 @@ export const NavBar = () => {
             </Nav>
             <span className="navbar-text">
               <div className="social-icon">
-                <a href="#"><img src={navIcon1} alt="" /></a>
-                <a href="https://www.facebook.com/FelixG28"><img src={navIcon2} alt="" /></a>
-                <a href="#"><img src={navIcon3} alt="" /></a>
+                <a href="https://www.linkedin.com/in/felix-g-b9aaa6217/" target="_blank"><img src={navIcon1} alt="" /></a>
+                <a href="https://www.facebook.com/FelixG28" target="_blank"><img src={navIcon2} alt="" /></a>
+                <a href="https://www.instagram.com/glix29/" target="_blank"><img src={navIcon3} alt="" /></a>
               </div>
               <HashLink to='#connect'>
                 <button className="vvd"><span>Let’s Connect</span></button>
