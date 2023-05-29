081aeeba94c391d1920ad1b9ff9f00168ef2969a
Lix2022 <98506225+Lix2022@users.noreply.github.com>
2023-05-29 02:56:43 +0000

    navbar-upted-code

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,13 +4,9 @@ import logo from '../assets/img/logoFG.svg';
 import navIcon1 from '../assets/img/nav-icon1.svg';
 import navIcon2 from '../assets/img/nav-icon2.svg';
 import navIcon3 from '../assets/img/nav-icon3.svg';
-import { HashLink } from 'react-router-hash-link';
-import {
-  BrowserRouter as Router
-} from "react-router-dom";
+import { BrowserRouter as Router, Link } from 'react-router-dom';
 
 export const NavBar = () => {
-
   const [activeLink, setActiveLink] = useState('home');
   const [scrolled, setScrolled] = useState(false);
 
@@ -37,7 +33,7 @@ export const NavBar = () => {
       <Navbar expand="sm" className={scrolled ? "scrolled" : ""}>
         <Container>
           <Navbar.Brand href="https://lix2022.github.io/My-Personal-Portfolio/">
-          <img src={logo} alt="Logo" className="logo-img" />
+            <img src={logo} alt="Logo" className="logo-img" />
           </Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav">
             <span className="navbar-toggler-icon"></span>
@@ -47,7 +43,6 @@ export const NavBar = () => {
               <Nav.Link href="#home" className={activeLink === 'home' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('home')}>Home</Nav.Link>
               <Nav.Link href="#skills" className={activeLink === 'skills' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('skills')}>Skills</Nav.Link>
               <Nav.Link href="#project" className={activeLink === 'project' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('project')}>Projects</Nav.Link>
-      
             </Nav>
             <span className="navbar-text">
               <div className="social-icon">
@@ -55,14 +50,13 @@ export const NavBar = () => {
                 <a href="https://www.facebook.com/FelixG28" target="_blank"><img src={navIcon2} alt="" /></a>
                 <a href="https://www.instagram.com/glix29/" target="_blank"><img src={navIcon3} alt="" /></a>
               </div>
-              <HashLink to='#connect'>
+              <Link smooth to='lix2022.github.io/My-Personal-Portfolio/#connect'>
                 <button className="vvd"><span>Let’s Connect</span></button>
-                
-              </HashLink>
+              </Link>
             </span>
           </Navbar.Collapse>
         </Container>
       </Navbar>
     </Router>
-  )
-}
+  );
+};
